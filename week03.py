e97c3ed060d2280675277eea0833847cda1f5d67
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2026-03-17 17:39:18 +0900

    week03 axis

--- week03.py
+++ week03.py
@@ -1,10 +1,15 @@
 import numpy as np
-items = [40,7,99,-3]
+scores = np.array([
+    #국영수
+    [80,100,90],#A학생
+    [100,86,66],#B
+    [33,54,77] #C
+])
 
-print(np.sum(items))
-print(np.mean(items))
-print(np.max(items))
-print(np.min(items))
-print(np.median(items))
-print(np.var(items))
-print(np.std(items))
+print(f"전체평균 {np.mean(scores)}")
+
+#국어 평균
+#=> 이걸위해 열(축:axis)연산을 함
+
+print(f"국 영 수 과목별 평균: {np.mean(scores,axis=0)}")
+print(f"학생별 최고점수: {np.max(scores,axis=1)}")
