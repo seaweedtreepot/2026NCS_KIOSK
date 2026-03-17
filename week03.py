16e2e3e3ed24efada711ba00c213427210fc26ea
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2026-03-17 18:22:43 +0900

    week03 pandas3

--- week03.py
+++ week03.py
@@ -13,3 +13,6 @@ items = [
 ]
 df_items = pd.DataFrame(items, index=[1,2,3],columns=['a','b','c'])
 print(df_items)
+
+df_items_melt = (pd.melt(df_items).rename(columns={'variable':'var','value':'val'}).query('val >= 75'))
+print(df_items_melt)
