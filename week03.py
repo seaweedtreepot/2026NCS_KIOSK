46715b445eababa14a8d807e18150b5463ea92d5
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2026-03-17 18:57:30 +0900

    week03 pandas4

--- week03.py
+++ week03.py
@@ -1,7 +1,8 @@
 import seaborn as sns
-
+import pandas as pd
 mpg = sns.load_dataset('mpg')
-print(mpg.head(3))
-print(mpg.tail(3))
+#print(mpg.head(3))
+#print(mpg.iloc[:, [0,1]])
 
-print(mpg.sort_values('mpg'))
+#print (mpg.loc[:,'model_year':'origin'])
+print(mpg.at[4,'A'])
