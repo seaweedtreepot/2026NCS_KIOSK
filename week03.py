d3138cc30741387095730db67a75509f0f298baa
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2026-03-17 17:54:39 +0900

    week03 pandas2

--- week03.py
+++ week03.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import numpy as np
 
-items = {'a':[100, 90, 80],
-         'b':[70,85, 95],
-         'c':[60,95, 100]
-         }
+# items = {'a':[100, 90, 80],
+#          'b':[70,85, 95],
+#          'c':[60,95, 100]
+#          }
 
-df_items = pd.DataFrame(items,index=[1,2,3])
+items = [
+    [100,95,70],
+    [75,65,35],
+    [100,30,20]
+]
+df_items = pd.DataFrame(items, index=[1,2,3],columns=['a','b','c'])
 print(df_items)
