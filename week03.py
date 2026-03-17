759e692eb2ae1e3943209e1a1e87db6ca31d502b
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2026-03-17 17:51:42 +0900

    week03 pandas1

--- week03.py
+++ week03.py
@@ -1,15 +1,10 @@
+import pandas as pd
 import numpy as np
-scores = np.array([
-    #국영수
-    [80,100,90],#A학생
-    [100,86,66],#B
-    [33,54,77] #C
-])
 
-print(f"전체평균 {np.mean(scores)}")
+items = {'a':[100, 90, 80],
+         'b':[70,85, 95],
+         'c':[60,95, 100]
+         }
 
-#국어 평균
-#=> 이걸위해 열(축:axis)연산을 함
-
-print(f"국 영 수 과목별 평균: {np.mean(scores,axis=0)}")
-print(f"학생별 최고점수: {np.max(scores,axis=1)}")
+df_items = pd.DataFrame(items,index=[1,2,3])
+print(df_items)
