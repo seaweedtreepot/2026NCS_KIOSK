bad1d6d165c7047e0a26db1540dda1bd5e0db7df
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2026-03-17 18:48:41 +0900

    week03 pandas4

--- week03.py
+++ week03.py
@@ -1,18 +1,7 @@
-import pandas as pd
-import numpy as np
+import seaborn as sns
 
-# items = {'a':[100, 90, 80],
-#          'b':[70,85, 95],
-#          'c':[60,95, 100]
-#          }
+mpg = sns.load_dataset('mpg')
+print(mpg.head(3))
+print(mpg.tail(3))
 
-items = [
-    [100,95,70],
-    [75,65,35],
-    [100,30,20]
-]
-df_items = pd.DataFrame(items, index=[1,2,3],columns=['a','b','c'])
-print(df_items)
-
-df_items_melt = (pd.melt(df_items).rename(columns={'variable':'var','value':'val'}).query('val >= 75'))
-print(df_items_melt)
+print(mpg.sort_values('mpg'))
