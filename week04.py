6ad0f882650f32bfc79cf54b8360eac8e8f46a6d
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2026-03-24 17:15:36 +0900

    week04 pandas3-groupby

--- week04.py
+++ week04.py
@@ -1,12 +1,10 @@
 import seaborn as sns
+import pandas as pd
 #cars that made in eu with mpg is higher than 30
 #but, only print name of the car, mpg, country,
 #And, update the downloaded-origin-data
 mpg = sns.load_dataset('mpg')
-#print(mpg.info())
 
-mpg = (mpg.drop(columns=['cylinders', 'displacement','horsepower','weight','acceleration','model_year']))
-print(mpg.info())
-mpg = mpg[['mpg','origin','name']].query('origin == "europe" and mpg >= 30').sort_values('mpg',ascending=False)
-
-#print(mpg)
+group_mpg = pd.DataFrame(mpg.groupby(by='cylinders'))
+print(group_mpg)
+#print(mpg.groupby(by='origin'))
