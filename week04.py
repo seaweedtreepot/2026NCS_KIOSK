c6920d42be03c9b1e38741ce34cf39c3e17657d9
seaweedtreepot <80155351+seaweedtreepot@users.noreply.github.com>
2026-03-24 17:05:40 +0900

    week04 pandas2

--- week04.py
+++ week04.py
@@ -1,8 +1,12 @@
 import seaborn as sns
 #cars that made in eu with mpg is higher than 30
 #but, only print name of the car, mpg, country,
+#And, update the downloaded-origin-data
 mpg = sns.load_dataset('mpg')
+#print(mpg.info())
 
-ans = mpg[['mpg','origin','name']].query('origin == "europe" and mpg >= 30').sort_values('mpg',ascending=False)
+mpg = (mpg.drop(columns=['cylinders', 'displacement','horsepower','weight','acceleration','model_year']))
+print(mpg.info())
+mpg = mpg[['mpg','origin','name']].query('origin == "europe" and mpg >= 30').sort_values('mpg',ascending=False)
 
-print(ans)
+#print(mpg)
